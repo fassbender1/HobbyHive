11a38fb1120781dce2fa5e762ddbc48ce4b435b0
fassbender1 <civello6969@gmail.com>
2026-04-06 08:35:27 +0300

    g

--- HobbyHive/urls.py
+++ HobbyHive/urls.py
@@ -35,6 +35,7 @@ handler404 = 'common.views.custom_404'
 handler500 = 'common.views.custom_500'
 handler403 = 'common.views.custom_403'
 
-if settings.DEBUG:
+if settings.DEBUG is False:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
