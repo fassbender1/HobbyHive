1aea1a9b71b1add46fdfa01628063915003d4677
fassbender1 <civello6969@gmail.com>
2026-04-07 07:16:57 +0300

    g

--- HobbyHive/settings.py
+++ HobbyHive/settings.py
@@ -131,6 +131,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer', 
+    ],
 }
 
 # Internationalization
